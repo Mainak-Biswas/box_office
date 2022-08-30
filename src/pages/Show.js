5ae8d28adbaa234777cad4802a4047915130599b
Mainak Biswas <mainakbiswas9@gmail.com>
2022-08-30 15:40:58 +0530

    Added Loading and Error message to the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,15 +7,47 @@ function Show() {
 
   const [show, setShow] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=episodes&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+    // isMounted variable maintains that the variables are not being used after being unmounted since the variable states are asynchronous.
+
+    apiGet(`/shows/${id}?embed[]=episodes&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          // Until timeout ends "isLoading" will be true
+          if (isMounted) {
+            setShow(results);
+            setIsLoading(false);
+          }
+        }, 100);
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      // Clean up will set ismounted to false -> All the async vars will no longer run in background
+      isMounted = false;
+    };
   }, [id]);
   // Takes 2 args, callback function , Array of dependency
   // call back function will run when something in the array changes
 
   console.log('Show', show);
+
+  if (isLoading) {
+    // Until timeout ends "isLoading" will be true
+    return <div>Data is being Loaded</div>;
+  }
+  if (error) {
+    <div>Error Occured: {error}</div>;
+  }
   return <div>This is show Page: {id} </div>;
 }
 
