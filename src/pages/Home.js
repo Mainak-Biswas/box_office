879cb5bfe0c0947c656e21605a00dd06c76e0c13
Mainak Biswas <mainakbiswas9@gmail.com>
2022-08-30 07:01:23 +0530

    Added rendering and search config file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 function Home() {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onInputChange = ev => {
     setInput(ev.target.value);
@@ -10,16 +12,31 @@ function Home() {
 
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=girls
-
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => console.log(result));
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResult = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -33,6 +50,8 @@ function Home() {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+
+      {renderResult()}
     </MainPageLayout>
   );
 }
