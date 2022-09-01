7513a1528da3a99fa2ef9c70939f485280495eb3
Mainak Biswas <mainakbiswas9@gmail.com>
2022-09-01 06:16:58 +0530

    Added ServiceWorker.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './service-worker';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
