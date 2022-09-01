4db0d3be98e06ce18a1618b22e79a72b7c3236b8
Mainak Biswas <mainakbiswas9@gmail.com>
2022-09-01 05:33:11 +0530

    Added ServiceWorker.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,4 +15,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 serviceWorker.register();
-reportWebVitals();
